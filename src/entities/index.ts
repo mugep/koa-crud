9334e39ff8caffdd94e5fe2985a553fcaaa88807
Muhamad Gerald <geralptr21@gmail.com>
2021-01-26 05:44:40 +0700

    entities dan tsconfig

---
+++ src/entities/index.ts
@@ -0,0 +1,32 @@
+import * as Koa from 'koa';
+import { DefaultContext, DefaultState } from 'koa';
+import { createConnection, Connection } from 'typeorm'
+import { config } from 'dotenv'
+import { PostsEntity } from './posts.entity'
+import { UsersEntity } from './users.entity'
+import 'colors'
+import 'reflect-metadata'
+
+config();
+
+const { DB_HOST, DB_USER, DB_PASS } = process.env
+console.log({DB_HOST, DB_USER, DB_PASS});
+
+export const connectDB = async(
+    app: Koa<DefaultState, DefaultContext>
+    ): Promise<void> => {
+    const connection: Connection = await createConnection({
+        type: 'mysql',
+        username: DB_USER,
+        password: DB_PASS,
+        host: DB_HOST,
+        database: 'koa',
+        entities: [PostsEntity, UsersEntity]
+    })
+
+    await connection.synchronize(true)
+    .then(() => console.log('synchronized! with DB'.green.bold))
+    .catch(() => console.error('Faild to sync with DB'.red.bold))
+    
+    app.context.db = connection
+}
---
+++ src/entities/users.entity.ts
@@ -0,0 +1,44 @@
+import { type } from 'os'
+import {Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm'
+import { PostsEntity } from './posts.entity'
+import { SharedProp } from './sharedProp.entity'
+
+export enum UserType {
+    user = 'user',
+    admin = 'admin'
+}
+
+@Entity({name: 'users'})
+export class UsersEntity extends SharedProp {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({name: 'first_name', nullable: false})
+    firstName: string
+    
+    @Column({name: 'last_name', nullable: false})
+    lastName: string
+
+    @Column({name: 'birth_of_date', nullable: true, type: 'date'})
+    birthOfDate: Date
+
+    @Column({unique: true, nullable: false})
+    email: string
+
+    @Column({default: UserType.user, enum: UserType, type: 'enum'})
+    type: UserType
+
+    @Column({nullable: false})
+    password: string
+
+    @Column({nullable: false})
+    salt: string
+
+    @OneToMany(() => PostsEntity, (post: PostsEntity) => post.user, {
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+    })
+    posts: Array<PostsEntity>
+
+    accessToken?: string
+}
